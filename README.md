b8e07b104bef2e4cfd10b34f56c827527d848919
Mester Balázs <balazs.mester@gmail.com>
2013-03-26 21:00:38 +0100

    Initial commit.

--- README.md
+++
@@ -1,4 +0,0 @@
-panka
-=====
-
-patient register for my mother and my sister in the beauty shop
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Panka::Application.config.secret_token = '3a05a5a993390dbc50544bdce4997ff90640b21cd1990778d9fe22d684d6102f33fcd9a6da1963f6ad21ad94b0c443b389d07dc708cdcc7aea665f68f3a81dae'
