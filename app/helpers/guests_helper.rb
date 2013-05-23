765b3aa715962c179858c2982d9271af3e217e06
Mester Balázs <balazs.mester@gmail.com>
2013-05-23 16:41:41 +0200

    Cleaning the views introduced helper method.

--- app/helpers/guests_helper.rb
+++ app/helpers/guests_helper.rb
@@ -1,2 +1,9 @@
 module GuestsHelper
+  def get_guest_name_link_or_selector(guest)
+    unless (params[:mode])
+      link_to guest.name, guest_path(:id => guest.id)
+    else
+      link_to guest.name, new_examination_path(:guest_id => guest.id)
+    end
+  end
 end
